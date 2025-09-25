726e4ad9b5dcd5fba101a5dc8fcaa6d257d9247c
Alex Henry <tukkek@gmail.com>
2025-09-25 00:00:00 -0300

    Bundle HTML and CSS with Compose.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Tie
-
 Tie is a tiny library for using templates easily and elegantly as building-blocks for your web-application. As simple as tying a knot!
 
 It is a minimalistic, opinionated[^O] library but it is based on the native HTML design of the `<template>` tag and object-oriented-programming[^OOP] and as such should feel familiar to any web developer and integrate seamlessly with other technologies such as CSS and other frameworks, libraries and tool-chains.
@@ -9,20 +8,17 @@ Tie is a 100% pure Javascript library. No servers, frameworks or build-steps req
 [^O]: *What makes software opinionated?* https://imkylelambert.com/articles/opinionated-software
 [^OOP]: *Using classes* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 
-## Setup
-
+# Setup
 There are multiple ways to install Tie. You can:
 * Add it as a `git` submodule: `git submodule add https://github.com/tukkek/tie/ library/tie/`
 * Download the latest version as a package: https://github.com/tukkek/tie/archive/refs/heads/main.zip
 * Download the latest version as a script (right-click and save-as): https://raw.githubusercontent.com/tukkek/tie/main/tie.js
 * Import the latest version directly from a module: `import * as tie from 'https://tukkek.github.io/tie/tie.js'`
 
-## Demonstrations and commented examples
-
+# Demonstrations and commented examples
 Tie is a tiny, elegant library and as such simple examples should be enough to explore its features. The code for the demonstrations can be found here: https://github.com/tukkek/tie/tree/main/demonstrations/
 
-### Template management
-
+## Template management
 This demonstration shows the library's basic template-based operations, such as: creation; insertion; reacting to native Java Script events; and how to access the underlying HTML elements to write the POJO code you're already familiar with[^POJO].
 
 Open this link to see the demonstration in action: https://tukkek.github.io/tie/demonstrations/race/race.html
@@ -33,8 +29,7 @@ The comments in these two sections of code should provide a good overview of Tie
 
 [^POJO]: Plain-old Java Script object https://en.wikipedia.org/wiki/Plain_old_Java_object
 
-### Reactive features
-
+## Reactive features
 This next demonstration show-cases Tie's two-way reactive functionalty (HTML to Java Script and vice-versa). Access the demonstration here: https://tukkek.github.io/tie/demonstrations/react/react.html
 
 The library features being high-lighted in this case are:
@@ -47,8 +42,7 @@ Keep in mind that you can always access all the native Java Script features you 
 
 The full commented code is pretty-self explanatory in itself. You can read it here: https://github.com/tukkek/tie/blob/main/demonstrations/react/react.js
 
-### Clones as classes
-
+## Clones as classes
 This demonstration features the use of Clone sub-classes as building-blocks of a simple web application. Since Clones are normal Java Script classes, they can be used with common object-oriented techniques such as creating hierarchies, compositions, aggrgations...
 
 View it in action here: https://tukkek.github.io/tie/demonstrations/filter/filter.html
@@ -57,15 +51,23 @@ The demonstration has a Clone sub-class for the filter widget and another for ev
 
 You can read the code for the two classes here: https://github.com/tukkek/tie/blob/main/demonstrations/filter/filter.js
 
-## Documentation
+## Bundles
+The Compose function is availabe to provide a way to bundle HTML and CSS files to modular Tie-components. Their contents will be inserted in respective Template and Style tags with the class-name provided.
 
+~~~js
+import * as tiem from './libraries/tie/tie.js'
+await tiem.compose('my-component',import.meta.url)
+~~~
+
+In this example the contents of `my-component.html` and `my-component.css` in the same-path as the JS file will be inserted under `<template class='my-component'>` and `<style class='my-component'>`.
+
+# Documentation
 The demonstrations cover the majority of Tie's features but the API documentation thoroughly describes the library and can be used as a reference while programming. It also contains a few reminders and tips so it's worth reading in full (it's also pretty short, at around only a dozen contained methods or so).
 
 - The full `Clone` class API can be found here: https://tukkek.github.io/tie/documentation/Clone.html
 - Elements and default event types supported by `clone.listen()` and `clone.react()` are listed here: https://tukkek.github.io/tie/documentation/global.html
 
-## Performance
-
+# Performance
 Tie uses `element.querySelector()` or `element.querySelectorAll()` for most of its methods, so performance should be typical for most Java Script scenarios. If that's not enough, a cached version[^C] should be twice as fast.
 
 For `clone.trap()` and `clone.bind()`, Tie uses the native Proxy object. You can test its performance here: https://www.measurethat.net/Benchmarks/Show/6274/4/access-to-proxy-vs-object
--- tie.js
+++ tie.js
@@ -1,7 +1,7 @@
-/** 
- * Determines per-element default listener event types for 
- * "clone.listen()", "clone.deafen()", "clone.react()" 
- * and "clone.ignore()". 
+/**
+ * Determines per-element default listener event types for
+ * "clone.listen()", "clone.deafen()", "clone.react()"
+ * and "clone.ignore()".
  * <br><br>
  * The supported elements (and event types) are:
  * <ul>
@@ -22,33 +22,33 @@ class Binding{
   }
 }
 
-/** 
- * Manages a cloned instance of a template tag.  Note that any searches 
+/**
+ * Manages a cloned instance of a template tag.  Note that any searches
  * are only performed on the clone's sub-tree (other than the constructor's
- * CSS selector). 
- * 
- * @property {element} template A reference to the template element being 
+ * CSS selector).
+ *
+ * @property {element} template A reference to the template element being
  * cloned (will be referred to as the "cloned template").
- * @property {element} root A reference to the root element cloned 
+ * @property {element} root A reference to the root element cloned
  * from the template (referred as the "template's clone").
  * @property {map} bindings A map with arrays of call-back functions
  * by property names (registered with "clone.bind()").
  */
 export class Clone{
-  /** 
-   * Finds a template tag, stores it as "clone.template" and 
-   * clones its content as "clone.root". Note that clones must have a single 
-   * root element (direct child descendant). This can be easily achieved by 
+  /**
+   * Finds a template tag, stores it as "clone.template" and
+   * clones its content as "clone.root". Note that clones must have a single
+   * root element (direct child descendant). This can be easily achieved by
    * wrapping the template's content inside a "div" tag.
    * <br><br>
    * Nested templates can't be accessed until the parent tag has been
    * inserted into the DOM tree. If problematic, don't nest your template
    * tags to avoid this issue.
-   * 
+   *
    * @param {string} selector A CSS selector for the template tag to be cloned.
    * For example: "#mytemplate" or "template#mytemplate".
-   * @param {string} classname For your convenience, this class-name 
-   * is added to the root element of all clones. If set to "false", will 
+   * @param {string} classname For your convenience, this class-name
+   * is added to the root element of all clones. If set to "false", will
    * not add any class-name to the root element.
    */
   constructor(selector,classname='tie'){
@@ -60,17 +60,17 @@ export class Clone{
     if(classname) c.classList.add(classname)
     this.root=c
   }
-  
-  /** 
-   * Appends the root element to the template's parent 
+
+  /**
+   * Appends the root element to the template's parent
    * element by default. Returns this clone instance,
    * for call-chaining.
    * <br><br>
-   * Always create a new Clone instance for each clone to be managed. 
+   * Always create a new Clone instance for each clone to be managed.
    * "clone.create()" won't clone the template again by itself.
-   * 
-   * @param {element} parent If present, append to this 
-   * element instead. 
+   *
+   * @param {element} parent If present, append to this
+   * element instead.
    */
   create(parent=false){
     if(!parent) parent=this.template.parentNode
@@ -78,9 +78,9 @@ export class Clone{
     return this
   }
 
-  /** 
+  /**
    * Listen for a default event from a single target-element.
-   * 
+   *
    * @param {string} selector Given a CSS selector...
    * @param {function} callback Registers a call-
    * back of the target-element's default event type.
@@ -89,11 +89,11 @@ export class Clone{
     let v=this.root.querySelector(selector)
     v.addEventListener(DEFAULTS.get(v.tagName),callback)
   }
-  
-  /** 
+
+  /**
    * Given the same parameters, removes a listener previously
-   * registered with "clone.listen()". 
-   * 
+   * registered with "clone.listen()".
+   *
    * @param {string} selector Same value as passed to "clone.listen()".
    * @param {function} callback Same value as passed to "clone.listen()".
    */
@@ -101,79 +101,79 @@ export class Clone{
     let v=this.root.querySelector(selector)
     v.removeEventListener(DEFAULTS.get(v.tagName),callback)
   }
-  
+
   /**
    * React to events from all interactive elements in the
    * clone's sub-tree. For example: to register a single function
    * that will render a preview when any input in a form is changed.
-   * 
-   * @param {function} callback Registers a call-back to all 
+   *
+   * @param {function} callback Registers a call-back to all
    * supported elements, using each element's default event type.
    */
   react(callback){
-    for(let d of DEFAULTS.keys()) 
+    for(let d of DEFAULTS.keys())
       for(let element of this.root.querySelectorAll(d))
         element.addEventListener(DEFAULTS.get(d),callback)
   }
-  
+
   /* Removes a call-back previously registered with "clone.react()". */
   ignore(callback){
     for(let d of DEFAULTS.keys())
       for(let element of this.root.querySelectorAll(d))
         element.removeEventListener(DEFAULTS.get(d),callback)
   }
-  
-  /** 
-   * @param {string} selector A CSS selector to find and return a 
-   * sub-tree element. 
+
+  /**
+   * @param {string} selector A CSS selector to find and return a
+   * sub-tree element.
    */
   select(selector){return this.root.querySelector(selector)}
-  
-  /** 
-   * @param {string} selector A CSS selector to find and return 
+
+  /**
+   * @param {string} selector A CSS selector to find and return
    * multiple sub-tree elements.
    */
   selectAll(selector){return this.root.querySelectorAll(selector)}
-  
+
   /** Lower-case alias for "clone.selectAll()". */
   selectall(selector){return this.selectAll(selector)}
-  
+
   /** Removes the root element from the DOM-tree. */
   remove(){this.root.remove()}
-  
-  /** 
+
+  /**
    * Use this to alter a standard HTML attribute (such as an anchor's "href")
-   * or to store data in an attribute declared anywhere in your sub-tree. 
+   * or to store data in an attribute declared anywhere in your sub-tree.
    * This can be useful in many cases, such as for selecting the element
-   * with the CSS query: "*[attribute='value']". 
+   * with the CSS query: "*[attribute='value']".
    * <br><br>
    * Note that the W3C suggests non-standard attribute names should have the
    * "data-' prefix.
-   * 
+   *
    * @param {string} attribute Find an element that has this HTML attribute...
    * @param {string} value Then set the attribute to this value.
    */
   set(attribute,value){this.select(`*[${attribute}]`).setAttribute(attribute,value)}
-  
+
   /**
    * @param {string} attribute Find an element that has this HTML attribute
    * and return the attribute's value.
    */
   get(attribute){return this.select(`*[${attribute}]`).getAttribute(attribute)}
-  
+
   /**
-   * Returns a proxy object whose properties can then be bound with 
+   * Returns a proxy object whose properties can then be bound with
    * "clone.bind()". Bound call-backs will only be called when using
-   * this method's returned proxy so it's good practice to discard the original 
+   * this method's returned proxy so it's good practice to discard the original
    * object's reference.
    * <br><br>
-   * Each clone instance is designed to trap a single object. Consider 
-   * using more clones or creating Proxy instances in client-code instead 
+   * Each clone instance is designed to trap a single object. Consider
+   * using more clones or creating Proxy instances in client-code instead
    * of calling this method more than once per instance.
    * <br><br>
    * For internal use, adds a "proxy.clone" reference to this clone object.
-   * 
-   * @param {object} target An object instance to trap. If not provided, will 
+   *
+   * @param {object} target An object instance to trap. If not provided, will
    * default to trapping this clone instance. Can be used to trap a different
    * data or control object as well, using the clone to react to its changes.
    */
@@ -184,15 +184,15 @@ export class Clone{
   }
 
   /**
-   * Binds a call-back to an object's property. The object must have been 
-   * previously-trapped with "clone.trap()". Any number of properties 
-   * and call-backs can be registered, including multiple call-backs for 
+   * Binds a call-back to an object's property. The object must have been
+   * previously-trapped with "clone.trap()". Any number of properties
+   * and call-backs can be registered, including multiple call-backs for
    * the same property.
-   * 
-   * @param {string} property A property name. For example: a property 
+   *
+   * @param {string} property A property name. For example: a property
    * "p" for a previously-trapped "o" object instance will bind "o.p".
-   * @param {function} callback Will be called whenever the bound-property 
-   * of the previously-trapped-object is changed. This function will be 
+   * @param {function} callback Will be called whenever the bound-property
+   * of the previously-trapped-object is changed. This function will be
    * called with the same arguments as: https://mdn.io/handler.set()
    */
   bind(property,callback){
@@ -204,10 +204,10 @@ export class Clone{
     }
     callbacks.push(callback)
   }
-  
+
   /**
    * Removes a call-back binding registered with "clone.bind()".
-   * 
+   *
    * @param {string} property Same value as passed to "clone.bind()".
    * @param {function} callback Same value as passed to "clone.bind()".
    */
@@ -219,4 +219,17 @@ export class Clone{
     callbacks.splice(i,1)
     return true
   }
+}
+
+async function load(name,extension,tag,parent){
+  let element=document.createElement(tag)
+  element.classList.add(name)
+  let body=await fetch(new URL(`${name}.${extension}`,parent))
+  element.innerHTML=await body.text()
+  document.body.appendChild(element)
+}
+
+export async function compose(name,parent){
+  await load(name,'html','template',parent)
+  await load(name,'css','style',parent)
 }
